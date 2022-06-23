1afe1b10adbbbc5e60b59646ec279e09860ea6f9
onatcipli <onatcipli@outlook.com>
2022-06-23 06:37:28 +0300

    changes to run ExoPlayer with HLS

--- app/src/main/java/com/example/exoplayer/MainActivity.kt
+++ app/src/main/java/com/example/exoplayer/MainActivity.kt
@@ -1,84 +1,57 @@
 package com.example.exoplayer
 
-import android.net.Uri
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import android.view.ViewParent
-import android.view.WindowManager
-import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.exoplayer.databinding.ActivityMainBinding
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.PlaybackException
-import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.source.MediaSource
-import com.google.android.exoplayer2.source.hls.DefaultHlsDataSourceFactory
+import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
-import com.google.android.exoplayer2.ui.PlayerControlView
-import com.google.android.exoplayer2.ui.PlayerView
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var exoPlayer: PlayerView
-    private lateinit var exoplayerParent: ConstraintLayout
+    private var exoPlayer: ExoPlayer? = null
+    private var playbackPosition = 0L
+    private var playWhenReady = true
 
-    private lateinit var simpleExoPlayer: SimpleExoPlayer
+    companion object {
+        const val URL =
+            "https://b356b5.entrypoint.cloud.wowza.com/app-9hkT7463/ngrp:544d4519_all/playlist.m3u8"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-
-        findViews()
-        createExoPlayer()
+        val view = binding.root
+        setContentView(view)
+        preparePlayer()
     }
 
-    private var playerListener = object : Player.Listener {
-        override fun onPlayerError(error: PlaybackException) {
-            super.onPlayerError(error)
-            Toast.makeText(this@MainActivity, "${error.message}", Toast.LENGTH_SHORT)
+    private fun preparePlayer() {
+        exoPlayer = ExoPlayer.Builder(this).build()
+        exoPlayer?.playWhenReady = true
+        binding.playerView.player = exoPlayer
+        val defaultHttpDataSourceFactory = DefaultHttpDataSource.Factory()
+        val mediaItem =
+            MediaItem.fromUri(URL)
+        val mediaSource =
+            HlsMediaSource.Factory(defaultHttpDataSourceFactory).createMediaSource(mediaItem)
+        exoPlayer?.setMediaSource(mediaSource)
+        exoPlayer?.seekTo(playbackPosition)
+        exoPlayer?.playWhenReady = playWhenReady
+        val playerListener = object :Player.Listener {
+        }
+        exoPlayer?.addListener(playerListener)
+        exoPlayer?.prepare()
+    }
+
+    private fun releasePlayer() {
+        exoPlayer?.let { player ->
+            playbackPosition = player.currentPosition
+            playWhenReady = player.playWhenReady
+            player.release()
+            exoPlayer = null
         }
     }
-
-    private fun createExoPlayer() {
-        simpleExoPlayer = SimpleExoPlayer.Builder(this).build()
-        simpleExoPlayer.addListener(playerListener)
-        val mediaSource: MediaSource = prepareMediaSource()
-
-        exoPlayer.player = simpleExoPlayer
-        simpleExoPlayer.addMediaSource(mediaSource)
-        simpleExoPlayer.prepare()
-    }
-
-    private fun prepareMediaSource(): MediaSource {
-        val url =
-            "https://b356b5.entrypoint.cloud.wowza.com/app-9hkT7463/ngrp:544d4519_all/playlist.m3u8"
-        val dataSourceFactory = DefaultDataSourceFactory(this, applicationInfo.name)
-        return HlsMediaSource.Factory(dataSourceFactory)
-            .createMediaSource(MediaItem.fromUri(Uri.parse(url)))
-    }
-
-    private fun findViews() {
-        exoplayerParent = findViewById(R.id.exoPlayerParent)
-        exoPlayer = findViewById(R.id.exoPlayer)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-    }
-
 }
